1e82c4e780ba411ca0ea672d5e2011a564390c21
PS0403 <pazlsalvador@gmail.com>
2023-11-11 15:35:04 -0300

    Entrepreneur class

--- back-end/src/main/java/com/example/back/model/Entrepreneur.java
+++ back-end/src/main/java/com/example/back/model/Entrepreneur.java
@@ -1,9 +1,12 @@
 package com.example.back.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.example.back.model.Course;
 
 import java.util.List;
 
@@ -16,17 +19,33 @@ public class Entrepreneur {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    
     @Column(nullable = false)
     private String name;
-
+    
+    @Column(nullable = false)
+    private LocalDateTime dob;
+    
     @Column(nullable = false)
     private String lastName;
+    
+    @Column(unique = true, nullable = false)
+    private String phoneNumber;
+    
+    @Column()
+    private String neighbourhood;
 
+    @Column()
+    private String city;
+    
+    @Column()
+    private String province;
+    
+    @Column()
+    private String educationLevel;
+    
     @Column(unique = true, nullable = false)
     private String email;
-
-    @Column(nullable = false)
-    private String phoneNumber;
 
     @Column(nullable = false)
     private String password;
